52e7ce2e7cd3a648bd87bd4d9dcc822e912453aa
Brian <brajai@hotmail.com>
2025-08-11 22:14:31 -0600

    fix use decimal library to expand the limit value of n (now 33)

--- cube_states.py
+++ cube_states.py
@@ -1,18 +1,22 @@
 import math
+from decimal import Decimal, getcontext
+
+# Establece la precisión para Decimal
+getcontext().prec = 3000
 
 def is_even(n):
     return n % 2 == 0
 
 def states(n):
-    if (is_even(n)):
-        k = n / 2
-        result = math.factorial(7) * pow(3 , 6) * pow(math.factorial(24), k * (k - 1)) / pow(math.factorial(4), 6 * (k - 1) ** 2)
+    if is_even(n):
+        k = Decimal(n) / 2
+        result = Decimal(math.factorial(7)) * Decimal(3) ** 6 * Decimal(math.factorial(24)) ** (k * (k - 1)) / Decimal(math.factorial(4)) ** (6 * (k - 1) ** 2)
         return result
     else:
-        k = (n + 1) / 2
-        result = math.factorial(8) * pow(3, 7) * pow(2, 10) * math.factorial(12) * pow(math.factorial(24), k * (k - 2)) / pow(math.factorial(4), 6 * (k -1) * (k - 2))
+        k = Decimal(n + 1) / 2
+        result = Decimal(math.factorial(8)) * Decimal(3) ** 7 * Decimal(2) ** 10 * Decimal(math.factorial(12)) * Decimal(math.factorial(24)) ** (k * (k - 2)) / Decimal(math.factorial(4)) ** (6 * (k - 1) * (k - 2))
         return result
-    
+
 def main():
     cube_size = int(input('Type the value of n: '))
     total_states = states(cube_size)
