7d00ee37c9795052b9c2acb3706155aaf4adb4e5
Brian <brajai@hotmail.com>
2025-08-11 22:22:06 -0600

    fix transfor int into string to support larger numbers for the scientific notation

--- cube_states.py
+++ cube_states.py
@@ -6,7 +6,6 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 5000
 
-
 def is_even(n):
     return n % 2 == 0
 
@@ -19,13 +18,28 @@ def states(n):
         k = Decimal(n + 1) / 2
         result = Decimal(math.factorial(8)) * Decimal(3) ** 7 * Decimal(2) ** 10 * Decimal(math.factorial(12)) * Decimal(math.factorial(24)) ** (k * (k - 2)) / Decimal(math.factorial(4)) ** (6 * (k - 1) * (k - 2))
         return result
+    
+def to_scientific_notation(n, digits=3):
+    n_str = str(n)
+    exponent = len(n_str) - 1
+    if len(n_str) > digits + 1:
+        mantissa = n_str[0] + '.' + n_str[1:digits+1]
+    else:
+        mantissa = n_str
+        exponent = 0
+    return f"{mantissa}e+{exponent}"
 
 def main():
     cube_size = int(input('Type the value of n: '))
     total_states = states(cube_size)
 
     rounded_states = round(total_states)
-    formatted_states = f'{rounded_states:,}'.replace(',', ' ')
+
+    if cube_size >= 6:
+        formatted_states = to_scientific_notation(rounded_states, digits=3)
+    else:
+        formatted_states = f'{rounded_states:,}'.replace(',', ' ')
+
     print(f'Number of possible states in a cube {cube_size}x{cube_size}x{cube_size} = {formatted_states}\n')
     
 if __name__ == '__main__':
